4a53e64814ba8e00bbbe3e18f22c7d28e3bc3413
Sohaib Irfan <tihor.personal@gmail.com>
2022-08-08 23:25:34 +0530

    Styled Navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-import logo from "./../../assets/K.png";
+import logo from "./../../assets/K_light.png";
+import "./Nav.css";
 
 const Nav = () => {
   return (
@@ -8,6 +9,14 @@ const Nav = () => {
       <div className="nav-left">
         <img className="keyed-logo" src={logo} alt="logo" />
         <p className="keyed-logo-text">Keyed</p>
+      </div>
+      <div className="nav-right">
+        <a
+          target="_blank"
+          className="nav-key-link"
+          href="https://github.com/Tokenass"
+          rel="noreferrer"
+        >TOKEN</a>
       </div>
     </div>
   );
